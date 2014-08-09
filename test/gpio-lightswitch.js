50fb47466251a25b4c6961d94506a0209add120f
fivdi <fivdiweb@gmail.com>
2014-08-09 15:22:29 +0200

    fixed gpio-lightswitch test

--- test/gpio-lightswitch.js
+++ test/gpio-lightswitch.js
@@ -1,5 +1,8 @@
 var bot = require('../'),
-  button = new bot.Gpio(bot.pins.p9_23, {direction: bot.Gpio.IN}),
+  button = new bot.Gpio(bot.pins.p9_23, {
+    direction: bot.Gpio.IN,
+    pullType: bot.pullTypes.PULL_UP
+  }),
   led = new bot.Gpio(bot.pins.p9_26);
 
 bot.once('ready', [button, led], function () {
