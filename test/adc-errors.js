4706819ab110ddb68f27b638b6192ca1f311dca3
fivdi <fivdiweb@gmail.com>
2014-10-27 22:58:45 +0100

    gpio error handling

--- test/adc-errors.js
+++ test/adc-errors.js
@@ -2,7 +2,7 @@
 
 var bot = require('../'),
   ainBad = new bot.Ain(bot.pins.p8_07), // p8_07 doesn't support adc, expect error
-  ainGood = new bot.Ain(bot.pins.p9_36); // p9_36 (AIN5) support adc, should work
+  ainGood = new bot.Ain(bot.pins.p9_36); // p9_36 (AIN5) supports adc, should work
 
 ainBad.on('error', function(err) {
   console.log('expected error -> ' + err.stack);
--- test/pwm-errors.js
+++ test/pwm-errors.js
@@ -2,7 +2,7 @@
 
 var bot = require('../'),
   pwmBad = new bot.Pwm(bot.pins.p8_07), // p8_07 doesn't support pwm, expect error
-  pwmGood = new bot.Pwm(bot.pins.p9_42); // p9_42 support pwm, should work
+  pwmGood = new bot.Pwm(bot.pins.p9_42); // p9_42 supports pwm, should work
 
 pwmBad.on('error', function(err) {
   console.log('expected error -> ' + err.stack);
